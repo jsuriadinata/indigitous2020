9dbd1baef9f513e5182e994744539c579d859af0
briankim31415 <briankim31415@gmail.com>
2020-10-17 16:51:23 -0500

    Instagram page view

--- src/views/Splash.js
+++ src/views/Splash.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import InstagramEmbed from 'react-instagram-embed';
 import {
     BrowserRouter as Router,
     Switch,
@@ -21,7 +22,82 @@ const Splash = () => {
             <div class="jumbotron text-center">
                 <h1>Instagram</h1>
             </div>
-                <Link to="/view">View</Link>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h3 class="text-center">Top Liked Post</h3>
+                    </div>
+                    <div class="col">
+                        <h3 class="text-center">Top Commented Post</h3>
+                    </div>
+                </div>
+
+                <div class="row mt-3">
+                    <div class="col btn-group">
+                        <button type="button" class="btn btn-primary">Week</button>
+                        <button type="button" class="btn btn-primary">Month</button>
+                        <button type="button" class="btn btn-primary">All-Time</button>
+                    </div>
+                    <div class="col-md-1"></div>
+                    <div class="col btn-group">
+                        <button type="button" class="btn btn-primary">Week</button>
+                        <button type="button" class="btn btn-primary">Month</button>
+                        <button type="button" class="btn btn-primary">All-Time</button>
+                    </div>
+                </div>
+
+                <div class="row mt-3">
+                    <div class="col">
+                        <InstagramEmbed
+                            url='https://instagram.com/p/CGbD6dFnCfR/?utm_source=ig_web_copy_link'
+                            maxWidth={320}
+                            hideCaption={true}
+                            containerTagName='div'
+                            protocol=''
+                            injectScript
+                            onLoading={() => {}}
+                            onSuccess={() => {}}
+                            onAfterRender={() => {}}
+                            onFailure={() => {}}
+                        />
+                    </div>
+                    <div class="col">
+                        <dl>
+                            <dt>Date</dt>
+                            <dd>21 Jul 2018</dd>
+                            <dt class="mt-3">Likes</dt>
+                            <dd>14</dd>
+                            <dt class="mt-3">Comments</dt>
+                            <dd>14</dd>
+                        </dl>
+                    </div>
+                    <div class="col-md-1"></div>
+                    <div class="col">
+                        <InstagramEmbed
+                            url='https://instagram.com/p/CGF10IAhGFd/'
+                            maxWidth={320}
+                            hideCaption={true}
+                            containerTagName='div'
+                            protocol=''
+                            injectScript
+                            onLoading={() => {}}
+                            onSuccess={() => {}}
+                            onAfterRender={() => {}}
+                            onFailure={() => {}}
+                        />
+                    </div>
+                    <div class="col">
+                        <dl>
+                            <dt>Date</dt>
+                            <dd>11 Jul 2018</dd>
+                            <dt class="mt-3">Likes</dt>
+                            <dd>14</dd>
+                            <dt class="mt-3">Comments</dt>
+                            <dd>14</dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
